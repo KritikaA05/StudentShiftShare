0e97ee8cb6704a574dec735f292095f757adc5c9
Kritika <kritikamourya13777@gmail.com>
2025-01-08 18:09:41 +0530

    Add informational URLs for About Us, Contact Us, Privacy Policy, and Terms of Services

--- jobs/urls.py
+++ jobs/urls.py
@@ -20,4 +20,10 @@ urlpatterns = [
     path('jobs/<int:job_id>/apply/', views.apply_job, name='apply_job'),  # Apply for job
     path('jobs/<int:job_id>/applications/', views.view_applications, name='view_applications'),  # View applications for a job
     path('applications/<int:application_id>/', views.application_detail, name='application_detail'),  # View application details
+
+   # Informational URLs
+    path('about_us/', views.about_us, name='about_us'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms_of_services/', views.terms_of_services, name='terms_of_services'),
 ]
